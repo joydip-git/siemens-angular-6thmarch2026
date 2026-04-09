98d441b822503197dd54a1cc9dc60afaeafa26f4
joydip-git <joydip.mondal@gmail.com>
2026-04-09 15:50:21 +0530

    product service and interface updated

--- codes/day-4/pms-app/src/app/products/services/product.service.ts
+++ codes/day-4/pms-app/src/app/products/services/product.service.ts
@@ -4,15 +4,22 @@ import { Product } from "../models/product";
 import { IServiceContract } from "./service-contract";
 import { inject } from "@angular/core";
 import { PRODUCT_API_URL } from "../../config/constants";
+import { ApiResponse } from "../../shared/models/api-response";
+import { Observable } from "rxjs";
 
 export class ProductService implements IServiceContract<Product> {
+
     private http: HttpClient;
     constructor() {
         this.http = inject(HttpClient)
     }
-    getAll(): readonly Product[] {
-        //this.http.get<model>(PRODUCT_API_URL)
-        return productRecords as Readonly<Product[]>
+    
+    get(id: number): Observable<ApiResponse<Product>> {
+        return this.http.get<ApiResponse<Product>>(`${PRODUCT_API_URL}/${id}`)
+    }
+
+    getAll(): Observable<ApiResponse<Product[]>> {
+        return this.http.get<ApiResponse<Product[]>>(PRODUCT_API_URL)
     }
 
 }
--- codes/day-4/pms-app/src/app/products/services/service-contract.ts
+++ codes/day-4/pms-app/src/app/products/services/service-contract.ts
@@ -1,4 +1,10 @@
-export interface IServiceContract<T>{
+import { Observable } from "rxjs";
+import { ApiResponse } from "../../shared/models/api-response";
+import { Product } from "../models/product";
+
+export interface IServiceContract<T> {
     //getAll(): Readonly<T[]>;
-    getAll(): readonly T[];
+    //getAll(): readonly T[];
+    getAll(): Observable<ApiResponse<T[]>>;
+    get(id: number): Observable<ApiResponse<T>>;
 }
