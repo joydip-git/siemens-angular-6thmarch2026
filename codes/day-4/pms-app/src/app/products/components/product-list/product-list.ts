f94c61e1cd75d7b854ea2b3f295cf62f5ebe5462
joydip-git <joydip.mondal@gmail.com>
2026-04-09 12:16:42 +0530

    lifecycle

--- codes/day-4/pms-app/src/app/products/components/product-list/product-list.ts
+++ codes/day-4/pms-app/src/app/products/components/product-list/product-list.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product';
 import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from '../../pipes/product-filter-pipe';
@@ -11,12 +11,30 @@ import { IServiceContract } from '../../services/service-contract';
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnChanges, OnInit, OnDestroy {
   //products: readonly Product[] | undefined;
   products?: readonly Product[];
+  isLoadingOver = false
+  errorMessage = ''
   filterText = ''
 
   constructor(@Inject(SERVICE_TOKEN) private productSvc: IServiceContract<Product>) {
-    // this.products = this.productSvc.getAll()
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+  ngOnInit(): void {
+    console.log('init');
+    try {
+      this.products = this.productSvc.getAll()
+      this.isLoadingOver = true
+      this.errorMessage = ''
+    } catch (error: any) {
+      this.products = undefined
+      this.isLoadingOver = true
+      this.errorMessage = error.message
+    }
+  }
+  ngOnDestroy(): void {
   }
 }
